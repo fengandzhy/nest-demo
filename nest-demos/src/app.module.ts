3d9dbbcb85bd81b84dbfd21597159f8d7d416d2a
fengandzhy <fengandzhy@gmail.com>
2024-04-09 20:24:15 +1200

    test

--- nest-demos/src/app.module.ts
+++ nest-demos/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { CourseModule } from './course/course.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule, CourseModule],
 })
 export class AppModule {}
